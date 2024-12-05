905a3ab5316b59a3cd0e59a375b128b836cad7b5
Endre BALOGH <balogh_endre@gmail.hu>
2024-12-05 17:27:51 +0100

    error handling added

--- app/tsvreader.py
+++ app/tsvreader.py
@@ -17,23 +17,27 @@ def readTSV(inFileName, dstEntries, dstErrors):
         i = 0
         for row in tsv_reader:
             i = i+1
-            (o, m) = row[:2]
-            if m is None:
-                addError(i, 'missing meaning', dstErrors)
-            t = None
-            x = None
-            if len(row) > 2:
-                t = row[2]
-            else:
-                addError(i, 'missing word type', dstErrors)
-            if len(row) > 3 and len(row[3]) > 0:
-                x = row[3:]
-            
-            if o in dstEntries:
-                e = dstEntries[o]
-                e.addMeaning(m, t, x)
-            else:
-                e = Entry(o, m, t, x)
-            dstEntries[o] = e
+            try:
+                (o, m) = row[:2]
+                if m is None:
+                    addError(i, 'missing meaning', dstErrors)
+                t = None
+                x = None
+                if len(row) > 2:
+                    t = row[2]
+                else:
+                    addError(i, 'missing word type', dstErrors)
+                if len(row) > 3 and len(row[3]) > 0:
+                    x = row[3:]
+                
+                if o in dstEntries:
+                    e = dstEntries[o]
+                    e.addMeaning(m, t, x)
+                else:
+                    e = Entry(o, m, t, x)
+                dstEntries[o] = e
+            except:
+                print(f"ERROR: line {i} :: {row}")
+                addError(i, 'not enough tabs', dstErrors)
         
         print(f"ReadTSV :: {len(dstEntries)} entries added from {inFileName}, errors: {len(dstErrors)}")
