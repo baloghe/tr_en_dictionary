a48e099478040e86ae8079894711e8289e5e8687
Endre BALOGH <balogh_endre@gmail.hu>
2024-12-08 10:29:40 +0100

    writeout from groups

--- app/jsonwriter.py
+++ app/jsonwriter.py
@@ -1,10 +1,14 @@
 import json
 
-def writeJSON(outFileName, inInflection2Entries):
+def writeJSON(outFileName, inGroups):
     j = []
-    for e in inInflection2Entries.keys():
+    for (id,e) in inGroups.items():
         #j.append(e.toJSONObj())
-        j.append(e)
+        if e.isValid():
+            j = j + e.getInflections()
+            # print(len(j))
+            j.append(e.getHead())
+    j = list(set(j))
     print(f"{len(j)} inflections detected")
     with open(outFileName, "w", encoding='UTF-8') as outfile:
         json.dump(j, outfile, ensure_ascii=False)
