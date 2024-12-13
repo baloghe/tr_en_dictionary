f840c1e95a7146e12035876d542294e1a2adad71
Endre BALOGH <balogh_endre@gmail.hu>
2024-12-13 17:51:14 +0100

    checkWordExistence added

--- proof/trydict.py
+++ proof/trydict.py
@@ -2,6 +2,7 @@ import json
 import re
 
 from app.tsvreader import readTSV
+from app.constants import alphaSort
 
 def getInflections(inJsonFileName):
     with open(inJsonFileName, 'r', encoding='UTF-8') as f:
@@ -19,10 +20,20 @@ def getDistinctWords(inText):
                 dword[w] = 1
     
     return dword
-    
+
+def checkWordExistence(inWord, inEntries, inInfls):
+    if inWord in inEntries.keys():
+        print(f"ENTRIES contain {inWord}")
+    else:
+        print(f"No such word in ENTRIES: {inWord}")
+    if inWord in inInfls:
+        print(f"Inflections contain {inWord}")
+    else:
+        print(f"No such word in Inflections: {inWord}")
+
 def writeResult(outFileName, inArray):
     file = open(outFileName, 'w', encoding='UTF-8')
-    file.writelines(list(map(lambda e: e[0] + '\t' + str(e[1]) + '\n', inArray)))
+    file.writelines(list(map(lambda e: e[0] + '\t' + str(e[1]) + '\n', alphaSort(inArray))))
     file.close()
 
 def tryDict(inInfls, inText, outFoundFileName, outNotFoundFileName):
@@ -62,9 +73,26 @@ def tryDictTSV(inDictFileName, inText, outFoundFileName, outNotFoundFileName):
             print(f"ERROR: no inflection could be calculated for Entry={o}")
     INFL = []
     for (k,e) in list(ENTRIES.items()):
+        o = e.getOrig()
+        INFL.append(o)
         i = e.getInflections()
         for t in list(i.keys()):
             for g in list(i[t].keys()):
                 for j in i[t][g]['infl']:
                     INFL.append(j['infl'])
+    
+    # see if some words are present...
+    checkWordExistence('anlaşılmıştı', ENTRIES, INFL)
+    '''
+    print(f"anlaşılmak -> {ENTRIES['anlaşılmak']}")
+    i = ENTRIES['anlaşılmak'].getInflections()
+    print(f"keys len: {len(list(i.keys()))}")
+    for t in list(i.keys()):
+        for g in list(i[t].keys()):
+            print(f"{t}.{g} len: {len(i[t][g]['infl'])}")
+            for j in i[t][g]['infl']:
+                if j['infl']=='anlaşılmıştı':
+                    print(j)
+    print('search finished')
+    '''
     tryDict(INFL, inText, outFoundFileName, outNotFoundFileName)
