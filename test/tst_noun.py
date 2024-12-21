8c667ea9cb8db1783f48a851c35b32bfc81577ee
Endre BALOGH <balogh_endre@gmail.hu>
2024-12-21 09:01:44 +0100

    enhance tester

--- test/tst_noun.py
+++ test/tst_noun.py
@@ -1,4 +1,21 @@
 from app.prcnoun import processNoun
 
-for w in ['gece']:
-    print(f"{w} -> {processNoun(w)}")
+for w in ['saç']:
+    pn = processNoun(w)
+    #print(f"{w} -> {pn}")
+    tofind = 'saçım'
+    srcTofind = 'Gen'
+    found = []
+    srcFound = []
+    
+    for g in pn.keys():
+        for i in pn[g]['infl']:
+            #print(i)
+            if tofind:
+                if i['infl'] == tofind:
+                    found.append(i)
+            if srcTofind:
+                if i['src'] == srcTofind:
+                    srcFound.append(i)
+    print(f"{tofind} -> {found}")
+    print(f"{srcTofind} -> {srcFound}")
