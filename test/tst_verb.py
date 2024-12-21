bd11640db423a0d9a37ea5f25b23241e747f4b5a
Endre BALOGH <balogh_endre@gmail.hu>
2024-12-21 08:59:59 +0100

    enhance tester

--- test/tst_verb.py
+++ test/tst_verb.py
@@ -1,4 +1,22 @@
 from app.prcverb import processVerb
 
-for w in ['yakalamak']:
-    print(f"{w} -> {processVerb(w)}")
+#for w in ['çıkarmak', 'edebileceğim', 'aramak', 'kaçmak', 'hatırlamak']:
+for w in ['kalmak']:
+    pn = processVerb(w)
+    #print(f"{w} -> {pn}")
+    tofind = 'kalırım'
+    srcTofind = 'Aor'
+    found = []
+    srcFound = []
+    
+    for g in pn.keys():
+        for i in pn[g]['infl']:
+            #print(i)
+            if tofind:
+                if i['infl'] == tofind:
+                    found.append(i)
+            if srcTofind:
+                if i['src'] == srcTofind:
+                    srcFound.append(i)
+    print(f"{tofind} -> {found}")
+    print(f"{srcFound} -> {srcFound}")
